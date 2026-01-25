40b5c722e33c779fc53e4c8d26aab08fd7509825
Bina Pithadiya <Bina@Binas-Macbook.local>
2026-01-25 12:15:22 +0530

    Implement list & view using mixings and generic views

--- watchmet/watchlist_app/api/urls.py
+++ watchmet/watchlist_app/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import WatchListAV, WatchListDetail, StreamPlatformList, StreamPlatformDetail
+from .views import WatchListAV, WatchListDetail, StreamPlatformList, StreamPlatformDetail, ReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', WatchListDetail.as_view(), name='movie-detail'),
     path('streamplatforms/', StreamPlatformList.as_view(), name='stream-platforms'),
-    path('streamplatforms/<int:pk>', StreamPlatformDetail.as_view(), name='stream-detail')
+    path('streamplatforms/<int:pk>', StreamPlatformDetail.as_view(), name='stream-detail'),
+    
+    path('reviews/', ReviewList.as_view(), name="review-list"),
+    path('reviews/<int:pk>', ReviewDetail.as_view(), name='review-detail')
 ]
